a76a771dffa3c07053f99aef0d9600df39c33498
christylmy29 <clmylmy1029@gmail.com>
2024-11-23 02:21:53 +0800

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,6 +1,17 @@
 #ifndef STATS_H
 #define STATS_H
 
-void stats();
+struct Statistics{
+    int TotalGamesPlay =0;
+    int RussianWon =0;
+    int RussianLosses =0;
+    int DemonWin =0;
+    int DemonLosses =0;
+    int totalTimePlayed =0; //in seconds
+};
 
+void SaveStat(Statistics&stat);
+void FindStat();
+void hardReset();
+void stats();
 #endif // STATS_H
