f11166c47326d5caa0ad3172c0c9f89139236395
mannixhung <lim523@connect.hku.hk>
2024-12-03 20:33:07 +0800

    Update Entity.cpp
    Add save and load function

--- Entity.cpp
+++ Entity.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include "Entity.h"
 #include "Inventory.h"
+#include <fstream>
 
 using namespace std;
 
@@ -52,5 +53,24 @@ void Entity::reducePunishedRounds(int amount) {
 
 Inventory& Entity::getInventory() {
     return *m_inventory;
+}
+
+void Entity::save(std::ofstream& fout) const{
+    size_t nameLength = name.size();
+    fout.write(reinterpret_cast<const char*>(&nameLength), sizeof(size_t));
+    fout.write(name.c_str(), nameLength);
+    fout.write(reinterpret_cast<const char*>(&maxLives), sizeof(int));
+    fout.write(reinterpret_cast<const char*>(&currentLives), sizeof(int));
+    inventory.save(fout);
+}
+
+void Entity::load(std::ifstream& fin) {
+    size_t nameLength;
+    fin.read(reinterpret_cast<char*>(&nameLength), sizeof(size_t));
+    name.resize(nameLength);
+    fin.read(&name[0], nameLength);
+    fin.read(reinterpret_cast<char*>(&maxLives), sizeof(int));
+    fin.read(reinterpret_cast<char*>(&currentLives), sizeof(int));
+    inventory.load(fin);
 }
 
