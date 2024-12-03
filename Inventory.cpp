b53bb47a75494f194ceb3f084843db755e75bd02
mannixhung <lim523@connect.hku.hk>
2024-12-03 21:57:50 +0800

    Update Inventory.cpp
    Add save and load to handle inventory data

--- Inventory.cpp
+++ Inventory.cpp
@@ -2,6 +2,7 @@
 #include "Item.h"
 #include <vector>
 #include <random>
+#include <fstream>
 
 using namespace std;
 
@@ -58,4 +59,29 @@ Item Inventory::getSlotItem(int slot) {
 
 int Inventory::getMaxInventorySlots() {
     return m_maxSlots;
+}
+
+void Inventory::save(std::ofstream& fout) const {
+    fout.write(reinterpret_cast<const char*>(&m_maxSlots), sizeof(int));
+
+    size_t itemCount = m_items.size();
+    fout.write(reinterpret_cast<const char*>(&itemCount), sizeof(size_t));
+
+    for (const auto& item : m_items) {
+        item.save(fout);
+    }
+}
+
+void Inventory::load(std::ifstream& fin) {
+    fin.read(reinterpret_cast<char*>(&m_maxSlots), sizeof(int));
+
+    size_t itemCount;
+    fin.read(reinterpret_cast<char*>(&itemCount), sizeof(size_t));
+
+    m_items.clear();
+    for (size_t i = 0; i < itemCount; ++i) {
+        Item item(ItemType::SAW); // Default item type, will be overwritten
+        item.load(fin);
+        m_items.push_back(item);
+    }
 }
