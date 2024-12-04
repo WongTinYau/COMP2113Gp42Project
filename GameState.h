ddcac9ccac6cb403839f96606e62ae351fa1e946
mannixhung <lim523@connect.hku.hk>
2024-12-05 06:44:15 +0800

    Update GameState.h

--- GameState.h
+++ GameState.h
@@ -8,14 +8,31 @@
 #include "Shotgun.h"
 
 struct GameState {
-  std::string currentLevel;
-  Entity player;
-  Entity dealer;
-  Shotgun shotgun;
-  bool isPlayerTurn;
-  std::vector<Item> dealerItemQueue;
-  int dealerNextChoice;
-  long totalPlayTime;
+    std::string currentLevel;
+    Entity player;
+    Entity dealer;
+    Shotgun shotgun;
+    bool isPlayerTurn;
+    std::vector<Item> dealerItemQueue;
+    int dealerNextChoice;
+    long totalPlayTime;
+
+    GameState(const std::string& level = "RussianDudeLevel", 
+              const Entity& playerEntity = Entity("Player", 2, 4),
+              const Entity& dealerEntity = Entity("Dealer", 2, 4),
+              const Shotgun& gun = Shotgun(2, 6, 1),
+              bool playerTurn = true,
+              const std::vector<Item>& itemQueue = {},
+              int nextChoice = -1,
+              long playTime = 0)
+        : currentLevel(level), 
+          player(playerEntity), 
+          dealer(dealerEntity), 
+          shotgun(gun), 
+          isPlayerTurn(playerTurn), 
+          dealerItemQueue(itemQueue), 
+          dealerNextChoice(nextChoice), 
+          totalPlayTime(playTime) {}
 };
 
 #endif
