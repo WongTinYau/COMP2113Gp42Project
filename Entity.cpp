21a7d2ea585826203258d13e5b1d0f20b1223890
WongTinYau <u3607130@connect.hku.hk>
2024-12-05 19:07:38 +0800

    Entity.cpp typo fix
    Fixed typos
    m_inventory.save(fout) still produces error (same for load)

--- Entity.cpp
+++ Entity.cpp
@@ -56,21 +56,21 @@ Inventory& Entity::getInventory() {
 }
 
 void Entity::save(std::ofstream& fout) const{
-    size_t nameLength = name.size();
+    size_t nameLength = m_displayName.size();
     fout.write(reinterpret_cast<const char*>(&nameLength), sizeof(size_t));
-    fout.write(name.c_str(), nameLength);
-    fout.write(reinterpret_cast<const char*>(&maxLives), sizeof(int));
-    fout.write(reinterpret_cast<const char*>(&currentLives), sizeof(int));
-    inventory.save(fout);
+    fout.write(m_displayName.c_str(), nameLength);
+    fout.write(reinterpret_cast<const char*>(&m_maxLives), sizeof(int));
+    fout.write(reinterpret_cast<const char*>(&m_currentLives), sizeof(int));
+    m_inventory.save(fout);
 }
 
 void Entity::load(std::ifstream& fin) {
     size_t nameLength;
     fin.read(reinterpret_cast<char*>(&nameLength), sizeof(size_t));
-    name.resize(nameLength);
-    fin.read(&name[0], nameLength);
-    fin.read(reinterpret_cast<char*>(&maxLives), sizeof(int));
-    fin.read(reinterpret_cast<char*>(&currentLives), sizeof(int));
-    inventory.load(fin);
+    m_displayName.resize(nameLength);
+    fin.read(&m_displayName[0], nameLength);
+    fin.read(reinterpret_cast<char*>(&m_maxLives), sizeof(int));
+    fin.read(reinterpret_cast<char*>(&m_currentLives), sizeof(int));
+    m_inventory.load(fin);
 }
 
